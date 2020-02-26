d435ed1f8898853136dbb570d2f6f54b30d79787
Chris Dzoba <chris@dzoba.com>
2020-02-26 14:17:25 -0800

    Move test

--- integration/all.test.js
+++ integration/all.test.js
@@ -1,6 +1,7 @@
 describe(`run recipes`, () => {
   describe(`placeholder`, () => {
     test(`test-1`, () => {
+      console.log("Test in integration folder")
       expect(true).toBe(true)
     })
   })

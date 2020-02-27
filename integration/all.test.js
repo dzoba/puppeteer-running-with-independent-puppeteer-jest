2d941dec99277e8317e6a3e9222be3685f972cb2
Chris Dzoba <chris@dzoba.com>
2020-02-26 16:12:04 -0800

    logs

--- integration/all.test.js
+++ integration/all.test.js
@@ -2,7 +2,9 @@ describe(`run recipes`, () => {
   describe(`placeholder`, () => {
     test(`test-1`, () => {
       console.log("Test in integration folder")
-      page.goto(`http://dzoba.com`, { waitUntil: `load` })
+      await page.goto(`http://dzoba.com`, { waitUntil: `load` })
+      console.log('%%%%%% Page', page)
+      console.log('%%%%%% Browser', browser)
       expect(true).toBe(true)
     })
   })

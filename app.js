7c756b5bde9dc7151d95a5e24df93fe7e939b43c
Chris Dzoba <chris@dzoba.com>
2020-03-17 17:36:11 -0700

    Test if this is actually chrome

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ const puppeteer = require(`puppeteer`);
     await page.goto("https://example.com");
     const title = await page.title();
     console.log(`%%%%% Independent puppeteer has retrieved title: ${title}`)
+    const isActuallyChrome = await page.evaluate(() => {
+      for (var i=0; i<navigator.plugins.length; i++){
+        if (navigator.plugins[i].name == 'Chrome PDF Viewer'){
+          return true;
+        }
+      }
+      return false;
+    });
+    console.log(`%%%%% isActuallyChrome: ${isActuallyChrome}`)
+
+    })
   // } catch (e) {
   //   console.log(e.message)
   // }
